60d413d9418fc0b0c19594a56333361458b6081d
Suraj12-tech <surajchauhan121414@gmail.com>
2026-07-10 13:56:23 +0530

    Time: 1 ms (45.99%), Space: 47.2 MB (16.43%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution { 
+    public int diameterOfBinaryTree(TreeNode root) {
+        int depth[] = new int [1];
+        check(root , depth);
+        return depth[0];
+    }
+
+    public int check(TreeNode node , int depth[]) {
+        if (node == null) {
+            return 0;
+        }
+
+        int lft = check(node.left,depth);
+        int rht = check(node.right,depth);
+
+        depth[0] = Math.max(depth[0], lft + rht);
+
+        return 1 + Math.max(lft, rht);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
